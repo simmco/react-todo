57bcbe9da8301b34231fcdfbaa8b03cddb155b17
Michael Simmelbauer <mas@Michaels-MacBook-Pro.local>
2016-11-17 11:45:42 +0100

    upload files

---
+++ src/app/index.js
@@ -0,0 +1,51 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./css/index.css');
+
+//Module requires
+var TodoItem = require('./todoItem');
+var AddItem = require('./addItem');
+
+//Create component
+var TodoComponent = React.createClass({
+  getInitialState: function() {
+    return {
+      todos: ['wash up','eat some cheese', 'take a nap']
+        }
+  },
+  render: function() {
+    var todos = this.state.todos;
+    todos = todos.map(function(item, index){
+      return(
+        <TodoItem item ={item} key={index} onDelete={this.onDelete}/>
+      );
+    }.bind(this));
+    return(
+      <div id="todo-list">
+        <p>The busiest people have the most leisure...</p>
+          <ul>{todos}</ul>
+          <AddItem onAdd={this.onAdd}/>
+      </div>
+    );
+  },// render
+  //Custom functions
+  onDelete: function(item){
+    var updatedTodos = this.state.todos.filter(function(val,index) {
+      return item !== val;
+    });
+    this.setState({
+      todos: updatedTodos
+    });
+  },
+  onAdd: function(item) {
+    var updatedTodos = this.state.todos;
+    updatedTodos.push(item);
+    this.setState({
+      todos: updatedTodos
+    })
+  }
+});
+
+
+//put component into html page
+ReactDOM.render(<TodoComponent />, document.getElementById("todo-wrapper"));
